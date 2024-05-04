eec4e6ba0095f24f91cf00e93d6b48f041452c99
MrFarhan <muhammadfarhan012345@gmail.com>
2024-05-04 23:26:05 +0500

    WIP - 3 tair Attendance System - Basic Structure

---
+++ BLL/Class1.cs
@@ -0,0 +1,13 @@
+﻿using DataAccessLayer;
+namespace BusinessLogicLayer
+{
+    public class bll_Student
+    {
+        public void InsertStudent()
+        {
+            DAL obj = new DAL();
+            obj.OpenConnection();
+        }
+            
+    }
+}
